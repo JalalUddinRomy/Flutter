5302b6fcbd40851d242d1ba927072dbd2aca44b5
JalalUddinRomy <smjalaluddin018251@gmail.com>
2025-02-28 02:27:12 +0600

    Initial commit

---
+++ Getter_Setter.dart
@@ -0,0 +1,10 @@
+
+import 'Getter_SetterMain.dart';
+
+void main(){
+  Worker Jalal=Worker();
+  print(Jalal.getSallary);
+  Jalal.setSallary=12000;
+  print("Jalal New sallary is ${Jalal.getSallary}");
+
+}
