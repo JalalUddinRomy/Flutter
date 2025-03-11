ee7cc188d16fed8e0d675ccbb2115407c94a831f
JalalUddinRomy <smjalaluddin018251@gmail.com>
2025-03-11 17:04:38 +0600

    Button and AlartDialog

---
+++ lib/button_alartdialog.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AlartActivity(),
+    );
+  }
+
+}
+
+class AlartActivity extends StatelessWidget {
+  MyAlartDialog(context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return Expanded(child: AlertDialog(
+            title: Text("Alart"),
+            content: Text("Do you want to leave"),
+            actions: [
+              TextButton(onPressed: (){Navigator.of(context).pop();}, child: Text("Yes")),
+              TextButton(onPressed: (){}, child: Text("NO"))
+            ],
+          ));
+
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Practicing Button and Alart Dialog"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: () {
+          MyAlartDialog(context);
+        }, child: Text("Click")),
+      ),
+    );
+  }
+
+}
