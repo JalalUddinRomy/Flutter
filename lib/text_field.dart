3d3b65e3466c7a7349915929d75510ef82cfbe32
JalalUddinRomy <smjalaluddin018251@gmail.com>
2025-03-16 04:08:59 +0600

    scrollview added

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//Text field,ScrollBar
 
 void main(){
   runApp(Myapp());
@@ -19,12 +20,15 @@ class FormActivity extends StatelessWidget{
         title: Text("Text Field"),
         backgroundColor: Colors.lightGreen,
       ),
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Name"),),),
-          Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Email"),),),
-          Padding(padding: EdgeInsets.all(5),child: ElevatedButton(onPressed: (){},style: buttonStyle, child:Text("Submit")),),
-        ],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Name"),),),
+            Padding(padding: EdgeInsets.all(10),child: TextField(decoration: InputDecoration(border: OutlineInputBorder(),labelText: "Email"),),),
+            Padding(padding: EdgeInsets.all(5),child: ElevatedButton(onPressed: (){},style: buttonStyle, child:Text("Submit")),),
+          ],
+        ),
       ),
     );
   }
