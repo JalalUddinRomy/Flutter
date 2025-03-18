e52bea45c123394903445cdf929a032e8a03b8f0
JalalUddinRomy <smjalaluddin018251@gmail.com>
2025-03-19 03:11:48 +0600

    container Image added

--- lib/container_row.dart
+++ lib/container_row.dart
@@ -17,23 +17,40 @@ class RowActivity extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:  AppBar(title:  Text("Container Practice"),backgroundColor: Colors.blueAccent,),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 250,
-            width: 250,
-            child: Text("My container"),
-            alignment:Alignment.center,
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.fromLTRB(12,13,14,15),
-            decoration: BoxDecoration(
-              color: Colors.deepOrange,
-              border: Border.all(width: 2,color: Colors.black),
-              borderRadius: BorderRadius.circular(10)
-            ),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              child: Text("My container"),
+              alignment:Alignment.bottomRight,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.fromLTRB(12,13,14,15),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(width: 2,color: Colors.black),
+                borderRadius: BorderRadius.only(
+                  bottomLeft:Radius.circular(16),
+                  topRight: Radius.circular(16)
+                ),
+                image: DecorationImage(
+                    image: AssetImage("assets/images/ajrin.jpeg"),fit: BoxFit.fill,opacity: 0.7),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.grey,
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: Offset(0, 3)
+                  )
+                ]
+              ),
+
+            )
+          ],
+        ),
       ),
     );
   }
